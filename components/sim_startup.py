810f6fa766c2461d2a18726457a020f230a1c573
Simeon Adesanya <simeon_a@Simeons-MacBook-Pro.local>
2023-09-27 17:49:45 +0100

    Add functions to setup simulation

---
+++ components/sim_startup.py
@@ -0,0 +1,70 @@
+"""Contains all functions related to setting up the simulation"""
+
+
+import sys
+import time
+
+
+WELCOME_MESSAGE = """Welcome to the shop simulator!
+This programme will simulate what it is like to run your own baking shop...
+Follow the instructions below to begin"""
+
+
+def message_in_terminal(message: str, delay: float = 0.05) -> None:
+    """Writes message to the terminal"""
+
+    for letter in message:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(delay)
+
+    print("\n")
+
+
+def pause_terminal(pause_time: float = 0.5) -> None:
+    """Sleep terminal for 0.5 seconds"""
+
+    time.sleep(pause_time)
+
+
+def setup_instructions() -> None:
+    """Prints the setup instructions in the terminal"""
+
+    message_in_terminal(
+        "You will begin with £10 worth of stock on your first day!")
+    pause_terminal()
+    message_in_terminal(
+        "You can choose a default inventory setup or a custom one...")
+    pause_terminal()
+
+
+def select_inventory_setup() -> str:
+    """Returns type of inventory setup based on user input"""
+
+    allowed_inventory_setups = {"default", "custom"}
+    inventory_setup = input("Choose default or custom inventory setup: ")
+
+    if inventory_setup.lower().strip() not in allowed_inventory_setups:
+        select_inventory_setup()
+
+    return inventory_setup.lower().strip()
+
+
+def run_simulation_start() -> None:
+    """Runs whole simulation"""
+
+    # message_in_terminal(WELCOME_MESSAGE)
+    # pause_terminal()
+    # setup_instructions()
+
+    inventory_setup = select_inventory_setup()
+
+    if inventory_setup == "default":
+        print("default selected")
+    else:
+        print("custom")
+
+
+if __name__ == "__main__":
+
+    run_simulation_start()
