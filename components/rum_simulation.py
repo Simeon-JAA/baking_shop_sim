64044cf923d435379bfdd358b03b9913abb562d2
Simeon Adesanya <simeon_a@Simeons-MacBook-Pro.local>
2023-09-29 17:28:03 +0100

    create run_simulation.py

---
+++ components/rum_simulation.py
@@ -0,0 +1,13 @@
+"""Handles all functions where the simulation runs"""
+
+
+from component_classes import Shop, User
+
+
+def run_simulation() -> None:
+    """Runs simulation"""
+    pass
+
+
+if __name__ == "__main__":
+    run_simulation()
